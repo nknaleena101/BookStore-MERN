2426d9be412371e1c1cb5d6628caf3af2bbd1446
nknaleena101 <nknaleena101@gmail.com>
2026-01-09 07:26:34 +0530

    conect mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.get("/", (request, response) => {
-    console.log(request)
-    return response.status(234).send("Hello from express server")
-} )
+  console.log(request);
+  return response.status(234).send("Hello from express server");    
+});
 
-app.listen(PORT, ()=> {
-    console.log(`app is listing to port: ${PORT}`)
-})
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connected to mongoDB");
+    app.listen(PORT, () => {
+      console.log(`app is listing to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
